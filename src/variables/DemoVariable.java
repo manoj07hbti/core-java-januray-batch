78a6415d14f835e9fbdc9c45ac4f17d6ba2e1abc
60manojkumar@gmail.com <Motivated@2019>
2022-01-19 19:32:50 +0530

    Variables code

---
+++ src/variables/DemoVariable.java
@@ -0,0 +1,26 @@
+package variables;
+
+public class DemoVariable {
+
+    public static void main(String[] args) {
+   //SYNTAX : DataType variable_name=value;
+
+      String  name="Java";
+
+      System.out.println("This is variable demo class ");
+      System.out.println(name);
+      String cityName="Agra";
+      System.out.println(cityName);
+
+      int age=23;
+      System.out.println(age);
+      int marks=75;
+      System.out.println(marks);
+
+      double price=34.5;
+      System.out.println(price);
+      double salary=37000.57;
+      System.out.println(salary);
+
+    }
+}
