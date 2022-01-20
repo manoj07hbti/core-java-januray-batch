76ad45e1852cf1c5e3a7202f70f73000fc92b1eb
60manojkumar@gmail.com <Motivated@2019>
2022-01-20 19:23:54 +0530

    if and else

---
+++ src/conditional_statements/ConditionalStatements.java
@@ -0,0 +1,20 @@
+package conditional_statements;
+
+public class ConditionalStatements {
+
+    public static void main(String[] args) {
+
+        //SYNTAX if() {CODE}
+
+        int age=19;
+
+        if(age>18){
+
+            System.out.println("Eligible for VOTING...");
+        }
+        else {
+            System.out.println("NOT Eligible for VOTING...");
+        }
+
+    }
+}
