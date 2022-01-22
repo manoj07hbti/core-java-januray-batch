7a3b1aaa8f7ca0f8b50d3482e6f90b16031c2d61
60manojkumar@gmail.com <Motivated@2019>
2022-01-22 19:21:25 +0530

    FOR LOOP

---
+++ src/loops/DemoLoop.java
@@ -0,0 +1,26 @@
+package loops;
+
+public class DemoLoop {
+
+
+    public static void main(String[] args) {
+
+   /*     LOOPS
+        for(initilization ;    condition;  increment/decrement){
+              code to be executed
+        }*/
+//INCREMENT
+        for (int i=0; i<5 ;  i++ ){
+
+            System.out.println("Hello World "+i);
+        }
+
+//DECREMENT
+      for (int i=5; i>0; i--) {
+
+          System.out.println("Decrement Hello World " + i);
+      }
+
+
+    }
+}
---
+++ src/loops/TableOfTwo.java
@@ -0,0 +1,16 @@
+package loops;
+
+public class TableOfTwo {
+
+    public static void main(String[] args) {
+
+        int number=2;
+
+        for (int i=1; i<=10; i++){
+
+            System.out.println("Printing Table "+number*i );
+            System.out.println("Value of i "+i );
+        }
+
+    }
+}
