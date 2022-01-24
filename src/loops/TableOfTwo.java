f059cab744982f0d512d1705c743696d7ee8cd98
60manojkumar@gmail.com <Motivated@2019>
2022-01-24 19:07:40 +0530

    loops

--- src/loops/TableOfTwo.java
+++ src/loops/TableOfTwo.java
@@ -8,8 +8,17 @@ public class TableOfTwo {
 
         for (int i=1; i<=10; i++){
 
-            System.out.println("Printing Table "+number*i );
-            System.out.println("Value of i "+i );
+
+            if(i==2){
+                System.out.println("2 Two are ");
+            }
+            if (i==3){
+                System.out.println("2 Three are ");
+            }
+            if(i==4){
+                System.out.println("2 Fours are ");
+            }
+            System.out.println(number*i );
         }
 
     }
