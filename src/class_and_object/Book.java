cba6924205d2e469d20cc004cf3aa813f0a2ca7b
60manojkumar@gmail.com <Motivated@2019>
2022-01-27 19:58:36 +0530

    class and object

---
+++ src/class_and_object/Book.java
@@ -0,0 +1,16 @@
+package class_and_object;
+
+public class Book {
+
+    //properties or DATA MEMBER
+
+    String name;
+    int pages;
+    double price;
+
+    // capabilities  or METHOD or FUNCTION
+    // read
+    // write
+
+
+}
---
+++ src/class_and_object/Student.java
@@ -0,0 +1,16 @@
+package class_and_object;
+
+public class Student {
+
+    // data member
+    String name;
+    int age;
+    String section;
+
+
+   //capabilities or method or function
+    // study
+    // play
+    // run
+
+}
